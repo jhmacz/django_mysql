c40eb9fc692060bb4e06dfd3a820ffd241508f4c
JohnSnow <john-henry-lumactud@hpe.com>
2023-06-06 00:05:00 +0800

    add signup validation

--- dcrm/website/forms.py
+++ dcrm/website/forms.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.forms imort UserCreationForm
-from django.contrib.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
     first_name = forms.CharField(label="", max_length=100,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
-    last_name =  form.CharField(label="", max_length=100,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
+    last_name =  forms.CharField(label="", max_length=100,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
 
     class Meta:
         model = User
